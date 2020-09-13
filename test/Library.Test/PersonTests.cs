6b21b9e8ce1c32e6493a46ded0e010e43756792a
Nicolas <nicolas.lozano.282002@gmail.com>
2020-09-12 21:18:12 -0300

    Update PersonTests.cs

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -32,5 +32,23 @@ namespace Tests
             Assert.AreEqual(resultadoesperado, name);
         
         }
+         [Test]
+        public void Id_Null() 
+        {
+            var cedula = this.person.ID;
+            var resultado = IdUtils.IdIsValid(cedula);
+            bool resultadoesperado = false;
+            Assert.AreEqual(resultadoesperado, cedula);
+        
+        }
+        [Test]
+        public void name_Null() 
+        {
+            var name = this.person.Name;
+            var resultado = IdUtils.IdIsValid(name);
+            bool resultadoesperado = false;
+            Assert.AreEqual(resultadoesperado, name);
+        
+        }
     }
 }
